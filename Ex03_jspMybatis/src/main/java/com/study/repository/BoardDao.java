3112a23b80e674a343798d20e22fd48957f2aa99
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-15 09:30:53 +0900

    Create BoardDao.java

---
+++ Ex03_jspMybatis/src/main/java/com/study/repository/BoardDao.java
@@ -0,0 +1,19 @@
+package com.study.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.study.dto.Board;
+
+//sql문(xml)과 메소드를 연결하고 결과값을 정의해놓은 타입으로 매핑 시켜주는것
+@Mapper
+public interface BoardDao {
+
+	public List<Board> list();  //메소드이름:list
+	public Board detailBoard(String no);  
+	public int totalRecord();  //토탈갯수
+	public int insertBoard(Board board);
+	public int deleteBoard(String no);
+
+}
