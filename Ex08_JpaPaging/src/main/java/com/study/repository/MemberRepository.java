ee0642ab1f3f752b1ffda0eaa97b3a88577ca290
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-18 17:45:37 +0900

    Create MemberRepository.java

---
+++ Ex08_JpaPaging/src/main/java/com/study/repository/MemberRepository.java
@@ -0,0 +1,18 @@
+package com.study.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.study.domain.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Page<Member> findByNameLike(String name, Pageable pageable);
+}
