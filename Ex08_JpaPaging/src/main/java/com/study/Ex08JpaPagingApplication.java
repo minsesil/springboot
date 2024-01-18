9f05c8c942e36a84f524e9743c508ef31ec80f82
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-18 17:45:34 +0900

    Create Ex08JpaPagingApplication.java

---
+++ Ex08_JpaPaging/src/main/java/com/study/Ex08JpaPagingApplication.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex08JpaPagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex08JpaPagingApplication.class, args);
+	}
+
+}
