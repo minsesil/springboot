d66f7f2cd0a18117a86f66aefad02cf1cf06f159
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-18 17:45:43 +0900

    Create MemberService.java

---
+++ Ex08_JpaPaging/src/main/java/com/study/service/MemberService.java
@@ -0,0 +1,21 @@
+package com.study.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.study.domain.Member;
+import com.study.repository.MemberRepository;
+
+@Service
+public class MemberService {
+	
+	@Autowired
+	private MemberRepository memberRepository;
+
+	public Page<Member> selectByNameLike(String name, Pageable pageable) {		
+		Page<Member> member = memberRepository.findByNameLike(name, pageable);
+		return member;
+	}
+}
