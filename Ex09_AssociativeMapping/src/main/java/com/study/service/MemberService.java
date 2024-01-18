721388cfe26e4fd7d64b239f7df97d36165c200f
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-18 17:46:39 +0900

    Create MemberService.java

---
+++ Ex09_AssociativeMapping/src/main/java/com/study/service/MemberService.java
@@ -0,0 +1,23 @@
+package com.study.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.study.domain.Member;
+import com.study.repository.MemberRepository;
+
+@Service
+public class MemberService {
+	
+	@Autowired
+	private MemberRepository memberRepository;
+
+
+
+	public Member insert(Member member) {
+		return memberRepository.save(member);
+		
+	}
+}
