cdb235c6ccc61c6c253dee87feb76a739d1961e1
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-18 17:47:32 +0900

    Create MemberRepository.java

---
+++ Ex0_MemberJpa02 - 복사본/src/main/java/com/study/repository/MemberRepository.java
@@ -0,0 +1,28 @@
+package com.study.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.study.domain.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByEmail(String email);
+
+	Optional<Member> findByName(String name);
+
+	List<Member> findByNameLike(String name);
+
+	List<Member> findByNameLikeOrderByNameDesc(String name);
+
+	//selectByNameLikeSort
+	List<Member> findByNameLike(String name, Sort sort);
+
+	List<Member> findByNameLikeSort(String name, Sort sort);
+
+}
