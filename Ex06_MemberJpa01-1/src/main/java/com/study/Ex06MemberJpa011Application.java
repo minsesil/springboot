96248717238c01e1782e593599086dd2a010f9be
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-18 17:41:35 +0900

    Create Ex06MemberJpa011Application.java

---
+++ Ex06_MemberJpa01-1/src/main/java/com/study/Ex06MemberJpa011Application.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex06MemberJpa011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex06MemberJpa011Application.class, args);
+	}
+
+}
