a22e7309e39848c534b9fa1a99a67f2f088f18c5
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-24 16:45:22 +0900

    Update BoardService.java

--- Ex03_jspMybatis/src/main/java/com/study/service/BoardService.java
+++ Ex03_jspMybatis/src/main/java/com/study/service/BoardService.java
@@ -5,10 +5,15 @@ import java.util.List;
 import com.study.dto.Board;
 
 public interface BoardService {
+	
 	public List<Board> list();  //board이 list목록 가져오기
+	
 	public Board detailBoard(String no);  
+	
 	public int totalRecord();  //토탈갯수
+	
 	public int insertBoard(Board board);
+	
 	public int deleteBoard(String no);
 	
 
