1ad9bd34d9f80569b30b1953122a6526b529ffe0
minsesil <148636941+minsesil@users.noreply.github.com>
2024-02-02 16:11:48 +0900

    2024.02.02

--- OnePlanner/src/main/java/com/study/service/EmailService.java
+++ OnePlanner/src/main/java/com/study/service/EmailService.java
@@ -1,52 +1,50 @@
+/*
+ * 이메일 전송과 관련된 기능을 제공하는 서비스 클래스
+ */
 package com.study.service;
 
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.springframework.mail.SimpleMailMessage; // SimpleMailMessage 가져오기
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
 	
-    private final JavaMailSender javaMailSender;
-    
-    //생성자 주입
-    //@Autowired
-    public EmailService(JavaMailSender javaMailSender) {
-    	this.javaMailSender = javaMailSender;
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    public void sendVerificationEmail(String to, String verificationCode) {
+    	
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        try {
+        	
+            helper.setTo(to);
+            helper.setSubject("[One Planner 이메일 인증]");  
+
+            // Thymeleaf를 사용하여 HTML 템플릿을 렌더링
+            Context context = new Context();
+            context.setVariable("verificationCode", verificationCode);
+
+            String htmlContent = templateEngine.process("verification/verification-email", context);  //verification/verification-email.html
+            helper.setText(htmlContent, true);   // true 전달하여 HTML 형식으로 설정
+
+            emailSender.send(message);
+            
+        } catch (MessagingException e) {
+            e.printStackTrace();  // 예외처리
+            
+        }
     }
-    
-    /*
-     * 간단한 텍스트 기반 이메일을 전송하는 메서드
-     * @param to : 수신자 이메일주소
-     * @param subject : 이메일 제목
-     * @param body : 이메일 내용
-     */
-    public void sendEmail(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);				//수신자 이메일 주소 설정
-        message.setSubject(subject);	//이메일 제목 설정
-        message.setText(body);			//이메일 내용 설정
-        javaMailSender.send(message);	//생성한 이메일 메시지를 전송
-    }
-    
-    /**
-     * HTML 기반 이메일을 전송하는 메서드
-     *
-     * @param to      수신자 이메일 주소
-     * @param subject 이메일 제목
-     * @param body    HTML 형식의 이메일 내용
-     */
-    public void sendHtmlEmail(String to, String subject, String body) throws MessagingException {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(body, true);
-        javaMailSender.send(message);
-    }    
+
 }
